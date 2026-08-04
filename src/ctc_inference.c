1ad1174a8fb2b1d59a591084a9df2d97ea935b0f
lucas-mior <lucas.mior@tutamail.com>
2026-08-04 16:07:37 -0300

    fix more clang warnings

--- src/ctc_inference.c
+++ src/ctc_inference.c
@@ -818,6 +818,7 @@ lrc_ctc_emissions_convert_to_log_probabilities(
             }
         }
         return true;
+    case LRC_CTC_EMISSION_VALUES_LAST:
     default:
         lrc_ctc_inference_result_set(
             result,
