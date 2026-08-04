df58ea62e78f074656451220614f03c5edb4635d
lucas-mior <lucas.mior@tutamail.com>
2026-08-04 16:07:05 -0300

    wip

--- src/ort.c
+++ src/ort.c
@@ -285,6 +285,7 @@ ort_session_options_configure_provider(
     case ORT_EXECUTION_PROVIDER_CUDA:
         required = true;
         return ort_session_options_append_cuda(context, options, required);
+    case ORT_EXECUTION_PROVIDER_LAST:
     default:
         lrc_progress_end_line();
         error2("unknown ONNX provider.\n");
